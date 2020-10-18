dbb984e50a05e018ed0e22fead744c33575c4d46
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-17 23:12:19 -0500

    complete structure

--- Spring00-LooselyCoupled/src/AppTest.java
+++ Spring00-LooselyCoupled/src/AppTest.java
@@ -1,12 +1,15 @@
 import service.FullTimeMentor;
 import service.MentorAccount;
+import service.PartTimeMentor;
 
 public class AppTest {
     public static void main(String[] args) {
 
-        FullTimeMentor fullTime = new FullTimeMentor();
+        //FullTimeMentor fullTime = new FullTimeMentor();
+        //MentorAccount mentor = new MentorAccount(fullTime);
 
-        MentorAccount mentor = new MentorAccount(fullTime);
+        PartTimeMentor partTime = new PartTimeMentor();
+        MentorAccount mentor = new MentorAccount(partTime);
 
         mentor.manageAccount();
         }

77f89ed7697750468fa493c24bf95daaef596b3b
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-18 01:13:33 -0500

    ApplicationContext added

--- Spring01-SpringContainer/src/main/java/AppTest.java
+++ Spring01-SpringContainer/src/main/java/AppTest.java
@@ -1,5 +1,6 @@
 import interfaces.Mentor;
 import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppTest {
@@ -11,11 +12,15 @@ public class AppTest {
 
         //Down cast
         Mentor mentorFullTime = (Mentor) container.getBean("fullTimeMentor");
-
         mentorFullTime.createAccount();
 
         Mentor mentorPartTime = (Mentor) container.getBean("partTimeMentor");
-
         mentorPartTime.createAccount();
+
+        //**************Another Way**********************************//
+
+        ApplicationContext container2 = new ClassPathXmlApplicationContext("config.xml");
+        //Instead of downcasting
+        Mentor mentor2 = container2.getBean("fullTimeMentor", Mentor.class);
     }
 }

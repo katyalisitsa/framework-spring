f6723367abf91616a9835f624f80457cdc9af730
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-20 18:12:48 -0500

    Dependency injection

---
+++ Spring02-DependencyInjection-ConstructorInjection/src/main/java/com/myApp/services/Java.java
@@ -0,0 +1,9 @@
+package com.myApp.services;
+
+import com.myApp.interfaces.Course;
+
+public class Java implements Course {
+    public void getTeachingHours() {
+        System.out.println("Weekly Teaching Hours: 20");
+    }
+}
---
+++ Spring02-DependencyInjection-ConstructorInjection/src/main/java/com/myApp/services/Selenium.java
@@ -0,0 +1,10 @@
+package com.myApp.services;
+
+import com.myApp.interfaces.Course;
+
+public class Selenium implements Course {
+    public void getTeachingHours() {
+            System.out.println("Weekly Teaching Hours: 15");
+    }
+
+}

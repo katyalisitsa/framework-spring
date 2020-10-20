1414badfd5cd2809e976d0cb1aaac57d0e8f7859
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-20 18:53:30 -0500

    injection

--- Spring02-DependencyInjection-ConstructorInjection/src/main/java/com/myApp/services/Java.java
+++ Spring02-DependencyInjection-ConstructorInjection/src/main/java/com/myApp/services/Java.java
@@ -9,6 +9,8 @@ public class Java implements Course {
     //Injection can be done through Constructor or Setters
     //Constructor injection - more popular - happens inside the Bean (Object into which another)
     //object needs to be injected
+    //Injection needed when object depends on another object
+    //Injection always needs to be done through interfaces
 
     OfficeHours officeHours;
 

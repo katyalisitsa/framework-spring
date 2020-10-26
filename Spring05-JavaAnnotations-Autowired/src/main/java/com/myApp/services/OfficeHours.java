a46ab9188b47ec6e19e263ce72ca02f79d47a2cc
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-26 11:35:47 -0400

    autowired annotation added to constructor

--- Spring05-JavaAnnotations-Autowired/src/main/java/com/myApp/services/OfficeHours.java
+++ Spring05-JavaAnnotations-Autowired/src/main/java/com/myApp/services/OfficeHours.java
@@ -1,7 +1,9 @@
 package com.myApp.services;
 
 import com.myApp.interfaces.ExtraSessions;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OfficeHours implements ExtraSessions {
     @Override
     public int getHours() {

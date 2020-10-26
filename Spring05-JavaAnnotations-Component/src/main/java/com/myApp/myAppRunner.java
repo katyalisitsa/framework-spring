7c1690185d58ebc46d83644b19c64f683edcdadf
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-26 10:42:55 -0400

    java annotations module added

---
+++ Spring05-JavaAnnotations-Component/src/main/java/com/myApp/myAppRunner.java
@@ -0,0 +1,4 @@
+package com.myApp;
+
+public class myAppRunner {
+}
---
+++ Spring05-JavaAnnotations-Component/src/main/java/com/myApp/services/Selenium.java
@@ -0,0 +1,11 @@
+package com.myApp.services;
+
+import com.myApp.interfaces.Course;
+
+public class Selenium implements Course {
+
+
+    public void getTeachingHours() {
+        System.out.println("Weekly teaching hours: 23");
+    }
+}

24b6ee013ea405c0b81126c423b79e199ab78d21
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-26 15:25:31 -0400

    config class added

--- Spring06-JavaCode-Configuration/src/main/java/com/myApp/myAppRunner.java
+++ Spring06-JavaCode-Configuration/src/main/java/com/myApp/myAppRunner.java
@@ -1,17 +1,19 @@
 package com.myApp;
 
+import com.myApp.configs.myAppConfig;
 import com.myApp.interfaces.Course;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class myAppRunner {
     public static void main(String[] args) {
 
-        ApplicationContext container = new ClassPathXmlApplicationContext("config.xml");
+        //can be more than one config file
+        ApplicationContext container = new AnnotationConfigApplicationContext(myAppConfig.class);
 
-        Course course = container.getBean("java", Course.class);
+        Course course = container.getBean("java",Course.class);
 
-        System.out.println(course.toString());
+        course.getTeachingHours();
 
     }
 }

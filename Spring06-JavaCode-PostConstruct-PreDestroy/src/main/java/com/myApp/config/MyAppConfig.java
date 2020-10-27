ae926e86c7d3507c87db88ac40fdf5aaba3027ef
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-27 19:28:58 -0400

    module with postconstruct and predestroy added

---
+++ Spring06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/myApp/config/MyAppConfig.java
@@ -0,0 +1,9 @@
+package com.myApp.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.myApp")
+public class MyAppConfig {
+}
---
+++ Spring06-JavaCode-PostConstruct-PreDestroy/src/main/java/com/myApp/myAppRunner.java
@@ -0,0 +1,20 @@
+package com.myApp;
+
+import com.myApp.interfaces.Course;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class myAppRunner {
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext("MyAppConfig.class");
+
+        Course course = container.getBean("java", Course.class);
+
+        course.getTeachingHours();
+        //System.out.println(course.toString());
+        ((AnnotationConfigApplicationContext)container).close();
+
+    }
+}

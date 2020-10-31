008bbd704b34f790b3bcc97a5975afb51ca622db
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-10-31 15:11:49 -0400

    spring boot lab

--- Spring07-SpringBoot-IoC-DI-Lab/src/main/java/com/myapp/services/carpet/Texas.java
+++ Spring07-SpringBoot-IoC-DI-Lab/src/main/java/com/myapp/services/carpet/Texas.java
@@ -2,25 +2,31 @@ package com.myapp.services.carpet;
 
 import com.myapp.enums.City;
 import com.myapp.interfaces.carpetPrices.Carpet;
+import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
 public class Texas implements Carpet {
-     private static final Map<City, BigDecimal> sqPriceForCity = new HashMap<>();
+
+    private static final Map<City, BigDecimal> sqPriceForCity = new HashMap<>();
 
     static {
-        sqPriceForCity.put(City.AUSTIN, new BigDecimal("2.92"));
-        sqPriceForCity.put(City.DULLES, new BigDecimal("1.32"));
-        sqPriceForCity.put(City.SAN_ANTONIO, new BigDecimal("2.65"));
+        sqPriceForCity.put(City.MCLEAN, new BigDecimal("4.32"));
+        sqPriceForCity.put(City.ARLINGTON, new BigDecimal("2.92"));
+        sqPriceForCity.put(City.FAIRFAX, new BigDecimal("4.65"));
     }
+
 
     @Override
     public BigDecimal getSqFtPrice(City city) {
-
         BigDecimal defaultValue = BigDecimal.ZERO;
 
         //Convert Map to Set objects
         //Optional incase value is null, or object in the memory doesn't exist
         Optional <Map.Entry<City, BigDecimal>> collect =  sqPriceForCity.entrySet().stream().filter(x -> x.getKey() == city).findFirst();
 
-        return collect.isPresent() ? collect.get().getValue() : defaultValue;
-    }
+        return collect.isPresent() ? collect.get().getValue() : defaultValue;    }
 }

aa72438547fafdbc9865b10c1362d0cf2d6bc593
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-12 14:46:57 -0500

    repo added

--- Spring09-ORM-TableCreation/src/main/java/com/myapp/repository/CarRepository.java
+++ Spring09-ORM-TableCreation/src/main/java/com/myapp/repository/CarRepository.java
@@ -2,6 +2,8 @@ package com.myapp.repository;
 
 import com.myapp.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
 }

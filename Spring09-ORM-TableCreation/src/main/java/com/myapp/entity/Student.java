34304031e461c5f991e7179f91aee441840505f7
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-12 13:44:58 -0500

    data pumped

---
+++ Spring09-ORM-TableCreation/src/main/java/com/myapp/entity/Student.java
@@ -0,0 +1,16 @@
+package com.myapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}

fc9ea51c82dcbc0d5bb17d20f9702af7937ae2c1
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-12 14:30:32 -0500

    inheritance

---
+++ Spring09-ORM-TableCreation/src/main/java/com/myapp/entity/Account.java
@@ -0,0 +1,15 @@
+package com.myapp.entity;
+
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import java.math.BigDecimal;
+
+@MappedSuperclass
+public class Account {
+
+    @Id
+    private Long id;
+    private String owner;
+    private BigDecimal balance;
+    private BigDecimal interestRate;
+}

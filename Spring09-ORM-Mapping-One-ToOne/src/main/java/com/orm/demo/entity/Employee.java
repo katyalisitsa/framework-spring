5f43a9b88266d7c37dd5c485d2ea6e6fe7532576
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-15 20:50:11 -0500

    OneToOne

--- Spring09-ORM-Mapping-One-ToOne/src/main/java/com/orm/demo/entity/Employee.java
+++ Spring09-ORM-Mapping-One-ToOne/src/main/java/com/orm/demo/entity/Employee.java
@@ -21,9 +21,13 @@ public class Employee extends BaseEntity{
     @Column(columnDefinition = "DATE")
     private LocalDate hireDate;
 
-    @OneToOne(cascade=CascadeType.ALL)
+    @OneToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
     @JoinColumn(name="department_id")
     private Department department;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="region_id")
+    private Region region;
 
     public Employee(String firstName, String lastName, String email, LocalDate hireDate, Gender gender, int salary) {
         this.firstName = firstName;

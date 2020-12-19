d7d138a56153c1f668872c0be8ab1021e3e50b05
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-19 13:52:26 -0500

    pump dummy data

--- Spring09-ORM-CinemaLab/src/main/java/com/cinemaapp/entity/Ticket.java
+++ Spring09-ORM-CinemaLab/src/main/java/com/cinemaapp/entity/Ticket.java
@@ -17,7 +17,7 @@ public class Ticket extends BaseEntity{
     private Integer rowNumber;
 
     @Column(columnDefinition = "TIMESTAMP")
-    private LocalDateTime localDateTime;
+    private LocalDateTime dateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="movie_cinema_id")
@@ -27,9 +27,9 @@ public class Ticket extends BaseEntity{
     @JoinColumn(name="user_account_id")
     private User user;
 
-    public Ticket(Integer seatNumber, Integer rowNumber, LocalDateTime localDateTime) {
+    public Ticket(Integer seatNumber, Integer rowNumber, LocalDateTime dateTime) {
         this.seatNumber = seatNumber;
         this.rowNumber = rowNumber;
-        this.localDateTime = localDateTime;
+        this.dateTime = dateTime;
     }
 }

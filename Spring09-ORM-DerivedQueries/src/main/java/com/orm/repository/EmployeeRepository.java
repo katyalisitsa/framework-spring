178adc2f7a4bfd7adb73cc885bae9ef8726bdfa3
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-22 21:03:31 -0500

    adding derived queries

--- Spring09-ORM-DerivedQueries/src/main/java/com/orm/repository/EmployeeRepository.java
+++ Spring09-ORM-DerivedQueries/src/main/java/com/orm/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@ import com.orm.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -23,4 +24,16 @@ public interface EmployeeRepository extends JpaRepository<Employee,String> {
 
     //Display all employees with salary higer than ''
     List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    //Display all employees that has been hired between '' and ''
+    List<Employee> findByHireDateBetween(LocalDate start, LocalDate end);
+
+    //Display all employees with salary greater and equal to '' in order
+    List<Employee> findBySalaryGreaterThanEqualOrderBy(Integer salary);
+
+    //Display top 3 unique employees that is making less than ''
+    List<Employee> findDistinctTop3BySalaryLessThan(Integer salary);
+
+    //Display all employees that do not have email address
+    List<Employee> findByEmailIsNull();
 }

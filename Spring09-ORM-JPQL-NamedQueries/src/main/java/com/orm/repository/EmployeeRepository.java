870d321fd7eaaa717cd9fbe7c7f1566e378b9c01
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-23 19:24:49 -0500

    JPQL Named queries added

--- Spring09-ORM-JPQL-NamedQueries/src/main/java/com/orm/repository/EmployeeRepository.java
+++ Spring09-ORM-JPQL-NamedQueries/src/main/java/com/orm/repository/EmployeeRepository.java
@@ -3,6 +3,7 @@ package com.orm.repository;
 import com.orm.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,8 +19,16 @@ public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     @Query("Select e from Employee e where e.email = ?1")
     Employee getEmployeeByEmail(String email);
 
-    //multiple bind parameter
+    //multiple bind parameters
     @Query("Select e from Employee e where e.email = ?1 and e.salary = ?2")
     Employee getEmployeeByEmailAndSalary(String email, Integer salary);
+
+    //single named parameter
+    @Query("Select e from Employee e weere e.salary = :salary")
+    Employee getEmployeeBySalary(@Param("salary") Integer salary);
+
+    ///multiple named parameters
+    @Query("Select e from Employee e where e.firstName=:name or e.salary=:salary")
+    List<Employee> getEmployeeByFirstNameOrSalary(@Param(firstName) String firstName, @Param(salary) Integer salary);
 
 }

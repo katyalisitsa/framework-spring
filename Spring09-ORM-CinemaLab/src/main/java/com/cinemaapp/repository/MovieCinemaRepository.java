756b8735ef5fe476b4f1c5deac95eaf49204556b
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2020-12-26 12:05:11 -0500

    more derived, jpql, antive queries

--- Spring09-ORM-CinemaLab/src/main/java/com/cinemaapp/repository/MovieCinemaRepository.java
+++ Spring09-ORM-CinemaLab/src/main/java/com/cinemaapp/repository/MovieCinemaRepository.java
@@ -2,8 +2,51 @@ package com.cinemaapp.repository;
 
 import com.cinemaapp.entity.MovieCinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Repository
 public interface MovieCinemaRepository extends JpaRepository<MovieCinema,Long> {
+
+    // ------------------- DERIVED QUERIES ------------------- //
+//Write a derived query to read movie cinema with id
+
+    List<MovieCinema> findMovieCinemasById(int id);
+
+//Write a derived query to count all movie cinemas with a specific cinema id
+
+    int countAllByCinemaId(Long cinemaId);
+
+//Write a derived query to count all movie cinemas with a specific movie id
+
+    Integer countAllByMovieId(Integer movieId);
+
+//Write a derived query to list all movie cinemas with higher than a specific date
+
+    List<MovieCinema> findByDateTimeAfter(LocalDateTime date);
+
+//Write a derived query to find the top 3 expensive movies
+
+    List<MovieCinema> findFirst3ByOrderByPMoviePrice();
+
+//Write a derived query to list all movie cinemas that contain a specific movie name
+
+    List<MovieCinema> findMovieCinemasByMovieNameContaining(String name);
+
+//Write a derived query to list all movie cinemas in a specific location
+
+    List<MovieCinema> findMovieCinemasByMCinemaLocationName(String location);
+
+// ------------------- JPQL QUERIES ------------------- //
+//Write a JPQL query to list all movie cinemas with higher than a specific date
+
+    @Query("Select mc from MovieCinema mc where mc.dateTime > ?1")
+    List<MovieCinema> fetchAllWithHigherSpecificDate(LocalDateTime dateTime);
+
+// ------------------- Native QUERIES ------------------- //
+//Write a native query to count all movie cinemas by cinema id
+//Write a native query that returns all movie cinemas by location name
 }

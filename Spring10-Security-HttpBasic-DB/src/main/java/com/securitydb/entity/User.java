a43ce492c963f6b8502123e3a0c4961ef3113b37
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-16 13:47:07 -0500

    BCrypt password encoder added

--- Spring10-Security-HttpBasic-DB/src/main/java/com/securitydb/entity/User.java
+++ Spring10-Security-HttpBasic-DB/src/main/java/com/securitydb/entity/User.java
@@ -5,6 +5,10 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 @Entity
 @Table(name = "users")
 @NoArgsConstructor
@@ -20,4 +24,26 @@ public class User {
     private String roles;
     private String permissions;
 
+    public User(String username, String password, String roles, String permissions) {
+        this.username = username;
+        this.password = password;
+        this.active = 1;
+        this.roles = roles;
+        this.permissions = permissions;
+    }
+
+    public List<String> getRoleList(){
+        if(this.roles.length()>0){
+            return Arrays.asList(this.roles.split(","));
+        }
+        return new ArrayList<>();
+    }
+
+    public List<String> getPermissionList(){
+        if(this.permissions.length()>0){
+            return Arrays.asList(this.permissions.split(","));
+        }
+        return new ArrayList<>();
+
+    }
 }

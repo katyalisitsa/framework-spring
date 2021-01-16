ee18eb05e4c42aa6e4e1a599ebed7d7b4ce51a80
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-16 10:50:20 -0500

    New security module added

--- Spring10-Security-HttpBasic-AuthorizationRoleBase/src/test/java/com/security/SecurityApplicationTests.java
+++ Spring10-Security-HttpBasic-AuthorizationRoleBase/src/test/java/com/security/SecurityApplicationTests.java
@@ -1,10 +1,10 @@
-package com.demo;
+package com.security;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SecurityApplicationTests {
 
     @Test
     void contextLoads() {

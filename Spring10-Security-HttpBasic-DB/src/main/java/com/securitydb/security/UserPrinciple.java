02749480a8d499a6b602475f3a484d5602256510
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-16 14:24:55 -0500

    UserDetails interface implemented

---
+++ Spring10-Security-HttpBasic-DB/src/main/java/com/securitydb/security/UserPrinciple.java
@@ -0,0 +1,68 @@
+package com.securitydb.security;
+
+import com.securitydb.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrinciple implements UserDetails {
+
+    private User user;
+
+    public UserPrinciple(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorityList = new ArrayList<>();
+
+        //Extract List of Permissions
+        user.getPermissionList().forEach(p -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority(p);
+            authorityList.add(authority);
+        });
+
+        //Extract List of Roles
+        user.getRoleList().forEach(r -> {
+            GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_" + r);
+            authorityList.add(authority);
+        });
+
+        return authorityList;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}

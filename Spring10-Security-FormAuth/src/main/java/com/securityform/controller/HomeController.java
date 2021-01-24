7e409451c88edf92b4a0cb5076678fd506da8be6
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-24 13:15:03 -0500

    Security COnfiguration

--- Spring10-Security-FormAuth/src/main/java/com/securityform/controller/HomeController.java
+++ Spring10-Security-FormAuth/src/main/java/com/securityform/controller/HomeController.java
@@ -5,11 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
 public class HomeController {
 
     @GetMapping("index")
     public String index() {
         return "index";
+    }
+
+    @GetMapping(value = {"/login", "/"})
+    public String login() {
+        return "login";
     }
 }

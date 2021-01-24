888cc4515a1f114eb1b0627daf9370a7f10b445b
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-24 12:06:08 -0500

    Spring security Auth form

---
+++ Spring10-Security-FormAuth/src/main/java/com/securityform/security/SecurityConfiguration.java
@@ -0,0 +1,31 @@
+package com.securityform.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("MANAGER", "ADMIN")
+                .and()
+                .httpBasic();
+    }
+    
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ Spring10-Security-FormAuth/src/test/java/com/securityform/SecurityformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.securityform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

0be90999709d7451c1ccf26ea8fefed6775f8381
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-25 18:56:56 -0500

    Service implemmentation added

---
+++ Spring11-Rest-HttpVerbs-Legacy/src/main/java/com/rest/service/ProductService.java
@@ -0,0 +1,20 @@
+package com.rest.service;
+
+import com.rest.entity.Product;
+
+import java.util.List;
+
+public interface ProductService {
+
+    List<Product> getProducts();
+
+    List<Product> delete(long id);
+
+    List<Product> updateProduct(long id, Product product);
+
+    List<Product> createProduct(Product product);
+
+    Product getProduct(long id);
+
+}
+

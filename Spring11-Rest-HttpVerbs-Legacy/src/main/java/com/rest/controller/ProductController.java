cc820467802ac1c5e940889de8a04fcbe9b0b5a6
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-25 20:04:42 -0500

    POST and PUT calls added

--- Spring11-Rest-HttpVerbs-Legacy/src/main/java/com/rest/controller/ProductController.java
+++ Spring11-Rest-HttpVerbs-Legacy/src/main/java/com/rest/controller/ProductController.java
@@ -1,8 +1,11 @@
 package com.rest.controller;
 
+import com.rest.entity.Product;
 import com.rest.service.ProductService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -12,4 +15,24 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
+    @RequestMapping(value = "products/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    Product getProduct(@PathVariable("id") long id) {
+        return productService.getProduct(id);
+    }
+
+    @RequestMapping(value = "products", method = RequestMethod.POST)
+    public @ResponseBody
+    List<Product> createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @RequestMapping(value = "products/{id}", method = RequestMethod.PUT)
+    public @ResponseBody
+    List<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+
+    }
+
 }

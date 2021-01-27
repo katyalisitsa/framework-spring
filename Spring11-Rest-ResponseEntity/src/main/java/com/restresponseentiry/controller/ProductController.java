c704a60c5ec24a36224cfcf0d83d5a0bf1870ae9
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-26 19:43:56 -0500

    Response entity module

---
+++ Spring11-Rest-ResponseEntity/src/main/java/com/restresponseentiry/controller/ProductController.java
@@ -0,0 +1,46 @@
+package com.restresponseentiry.controller;
+
+
+
+import com.restresponseentiry.entity.Product;
+import com.restresponseentiry.service.ProductService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping(value = "{id}")
+    public Product getProduct(@PathVariable("id") long id) {
+        return productService.getProduct(id);
+    }
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return productService.getProducts();
+    }
+
+    @PostMapping
+    public List<Product> createProduct(@RequestBody Product product) {
+        return productService.createProduct(product);
+    }
+
+    @PutMapping(value = "{id}")
+    public List<Product> updateProduct(@PathVariable("id") long id, @RequestBody Product product) {
+        return productService.updateProduct(id, product);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public List<Product> delete(@PathVariable("id") long id) {
+        return productService.delete(id);
+    }
+
+}
---
+++ Spring11-Rest-ResponseEntity/src/main/java/com/restresponseentiry/repository/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.restresponseentiry.repository;
+
+
+import com.restresponseentiry.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+}

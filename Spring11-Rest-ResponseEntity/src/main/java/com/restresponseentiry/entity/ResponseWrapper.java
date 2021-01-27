23cb930a09f423d8020537a57120542e201dbf1f
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-26 20:17:50 -0500

    Response Wrapper custom class

--- Spring11-Rest-ResponseEntity/src/main/java/com/restresponseentiry/entity/ResponseWrapper.java
+++ Spring11-Rest-ResponseEntity/src/main/java/com/restresponseentiry/entity/ResponseWrapper.java
@@ -3,6 +3,8 @@ package com.restresponseentiry.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
 
 @Getter
 @Setter
@@ -11,4 +13,19 @@ public class ResponseWrapper {
 
     private boolean success;
     private String message;
+    private Integer code;
+    private Object data;
+
+    public ResponseWrapper(String message, Object data) {
+        this.message = message;
+        this.data = data;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
+
+    public ResponseWrapper(String message) {
+        this.message = message;
+        this.code = HttpStatus.OK.value();
+        this.success = true;
+    }
 }

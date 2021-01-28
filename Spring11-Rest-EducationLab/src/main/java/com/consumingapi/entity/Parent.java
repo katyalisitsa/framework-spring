f4bd4c663f77b2f5390f9078dca9fa70a9fa51dc
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-27 21:17:35 -0500

    Lab with Consuming third party API

--- Spring11-Rest-EducationLab/src/main/java/com/consumingapi/entity/Parent.java
+++ Spring11-Rest-EducationLab/src/main/java/com/consumingapi/entity/Parent.java
@@ -3,12 +3,13 @@ package com.consumingapi.entity;
 import com.consumingapi.enums.Status;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 import java.time.LocalDate;
 
 @Getter
@@ -18,15 +19,25 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer"}, ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Parent extends BaseEntity{
+public class Parent extends BaseEntity {
 
     private LocalDate birthday;
     private String email;
     private String firstName;
+    private String lastName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
     private String phoneNumber;
     private String profession;
+
+    @Enumerated(EnumType.STRING)
     private Status status;
     private String username;
     private Long addressId;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id")
+    private Address address;
 }

0dfe708ce48cb808bc3e7af410df9dd1bfe1cc73
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-27 19:22:05 -0500

    Jackson module

--- Spring11-Rest-Jackson/src/main/java/com/jackson/repository/GenreRepository.java
+++ Spring11-Rest-Jackson/src/main/java/com/jackson/repository/GenreRepository.java
@@ -11,15 +11,15 @@ import java.util.List;
 @Repository
 public interface GenreRepository extends JpaRepository<Genre, Long> {
 
-    // ------------------- JPQL QUERIES ------------------- //
-//Write a JPQL query that return all genres
-
-    @Query("select g from genre g")
-    List<Genre> findGenres();
-
-// ------------------- Native QUERIES ------------------- //
-//Write a native query that returns genres by containing name
-
-    @Query(value = "Select * from genre where name is ilike concat('%',?1,'%')", nativeQuery = true)
-    List<Genre> findGenresByPatternInName(String pattern);
+//    // ------------------- JPQL QUERIES ------------------- //
+////Write a JPQL query that return all genres
+//
+//    @Query("select g from genre g")
+//    List<Genre> findGenres();
+//
+//// ------------------- Native QUERIES ------------------- //
+////Write a native query that returns genres by containing name
+//
+//    @Query(value = "Select * from genre where name is ilike concat('%',?1,'%')", nativeQuery = true)
+//    List<Genre> findGenresByPatternInName(String pattern);
 }

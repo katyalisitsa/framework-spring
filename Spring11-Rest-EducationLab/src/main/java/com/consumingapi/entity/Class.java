7d684308671e9bf9316ba1c9cd0e43d99770e50f
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-01-27 21:23:20 -0500

    Lab with Consuming third party API

--- Spring11-Rest-EducationLab/src/main/java/com/consumingapi/entity/Class.java
+++ Spring11-Rest-EducationLab/src/main/java/com/consumingapi/entity/Class.java
@@ -8,6 +8,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Getter
 @Setter
@@ -16,11 +19,17 @@ import javax.persistence.Entity;
 @AllArgsConstructor
 @JsonIgnoreProperties(value = {"hibernateLazyInitializer"}, ignoreUnknown = true)
 @JsonInclude(JsonInclude.Include.NON_NULL)
-public class Class extends BaseEntity{
+public class Class extends BaseEntity {
 
     private String name;
     private Integer year;
-    private Long courseId;
-    private Long teacherId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "teacher_id")
+    private Teacher teacher;
 
 }

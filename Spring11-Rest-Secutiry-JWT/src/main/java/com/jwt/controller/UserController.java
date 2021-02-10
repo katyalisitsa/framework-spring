6f26b49ca6b05697485a2a4a9656294430b83a9b
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-02-09 20:51:30 -0500

    Json Web Token Generation

--- Spring11-Rest-Secutiry-JWT/src/main/java/com/jwt/controller/UserController.java
+++ Spring11-Rest-Secutiry-JWT/src/main/java/com/jwt/controller/UserController.java
@@ -5,6 +5,7 @@ import com.jwt.entity.User;
 import com.jwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/users")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@PreAuthorize("hasAuthority('ADMIN')")
 public class UserController {
 
     @Autowired

adffb5cfa6b26e947145589f8721b909c96f505e
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-02-20 14:18:31 -0500

    AOP module

---
+++ Spring12-AOP/src/main/java/com/aop/bootstrap/DataGenerator.java
@@ -0,0 +1,32 @@
+package com.aop.bootstrap;
+
+
+
+import com.restresponseentiry.entity.Product;
+import com.restresponseentiry.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
+
+public class DataGenerator implements CommandLineRunner {
+
+    private ProductRepository productRepository;
+
+    public DataGenerator(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Product pc = new Product("Dell");
+        Product laptop = new Product("MacBook");
+        Product phone = new Product("IPhone");
+        Product tablet = new Product("IPad");
+
+        productRepository.save(pc);
+        productRepository.save(laptop);
+        productRepository.save(phone);
+        productRepository.save(tablet);
+
+
+    }
+}
---
+++ Spring12-AOP/src/main/java/com/aop/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.aop.service;
+
+import com.restresponseentiry.entity.Product;
+
+import java.util.List;
+
+
+public interface ProductService {
+
+    List<Product> getProducts();
+
+    List<Product> delete(long id);
+
+    List<Product> updateProduct(long id, Product product);
+
+    List<Product> createProduct(Product product);
+
+    Product getProduct(long id);
+
+}
+

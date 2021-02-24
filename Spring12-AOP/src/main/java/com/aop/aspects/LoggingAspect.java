a8101a996b472c89564d627f7ef3e52d0339b915
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-02-23 20:51:00 -0500

    Aspect Advice and Pointcut created

--- Spring12-AOP/src/main/java/com/aop/aspects/LoggingAspect.java
+++ Spring12-AOP/src/main/java/com/aop/aspects/LoggingAspect.java
@@ -59,4 +59,22 @@ public class LoggingAspect {
     private void anyServiceAnnotatedOperation() {
     }
 
+    @Before("anyServiceAnnotatedOperation() || anyControllerOperation() ")
+    public void beforeControllerAdvice2(JoinPoint joinPoint) {
+        logger.info("Before -> Method {} - Arguments : {} - Target : {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
+    }
+
+    //===========================ANNOTATION===========================
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    private void anyDeleteProductOperation() {
+    }
+
+    @Before("anyDeleteProductOperation()")
+    public void beforeControllerAdvice3(JoinPoint joinPoint) {
+        logger.info("Before -> Method {} - Arguments : {} - Target : {}", joinPoint, joinPoint.getArgs(), joinPoint.getTarget());
+
+    }
+
+
 }

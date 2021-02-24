3daf203d1cd562583eb703b0e4082c7c266e3805
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-02-23 21:14:06 -0500

    Aspect Advice and Pointcut created

--- Spring12-AOP/src/main/java/com/aop/aspects/LoggingAspect.java
+++ Spring12-AOP/src/main/java/com/aop/aspects/LoggingAspect.java
@@ -3,10 +3,7 @@ package com.aop.aspects;
 import com.aop.controller.ProductController;
 import com.aop.entity.Product;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.AfterReturning;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
@@ -96,4 +93,28 @@ public class LoggingAspect {
     public void afterReturningControllerAdvice2(JoinPoint joinPoint, List<Product> results) {
         logger.info("After Returning(List Results) -> Method : {} - results : {}", joinPoint.getSignature().toShortString());
     }
+
+    //===========================AFTER THROWING===========================
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void anyGetPutProductOperation() {
+    }
+
+    @AfterThrowing(pointcut = "anyGetPutProductOperation()", throwing = "exception")
+    public void afterThrowingControllerAdvice(JoinPoint joinPoint, RuntimeException exception) {
+        logger.info("After Throwing(Send error email) -> Method : {} - Exception : {}", joinPoint.getSignature().toShortString(), exception.getMessage());
+    }
+
+    //===========================AFTER===========================
+
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void anyGetPutProductOperation2() {
+    }
+
+    @After("anyGetPutProductOperation2()")
+    public void afterControllerAdvice(JoinPoint joinPoint) {
+        logger.info("After finally -> Method : {} - Exception : {}", joinPoint.getSignature().toShortString());
+    }
+
+
 }

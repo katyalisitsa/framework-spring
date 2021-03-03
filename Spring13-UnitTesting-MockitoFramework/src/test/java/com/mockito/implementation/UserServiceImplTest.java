d64505eda867f350fce2855359d01b378a027132
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-03-02 19:28:37 -0500

    Mockito Unit Testing

--- Spring13-UnitTesting-MockitoFramework/src/test/java/com/mockito/implementation/UserServiceImplTest.java
+++ Spring13-UnitTesting-MockitoFramework/src/test/java/com/mockito/implementation/UserServiceImplTest.java
@@ -1,14 +1,26 @@
 package com.mockito.implementation;
 
+import com.mockito.mapper.MapperUtil;
+import com.mockito.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceImplTest {
 
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    MapperUtil mapperUtil;
+
+    @InjectMocks
     UserServiceImpl userService;
 
     @Test
@@ -16,5 +28,7 @@ class UserServiceImplTest {
         userService.deleteByUserName("mike@gmail.com");
 
         //verification
+        verify(userRepository).deleteByUserName("mike@gmail.com");
+
     }
 }

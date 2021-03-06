ab445f68d7811f6f6e6822912b305fc0a58aade2
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-03-06 11:56:17 -0500

    MockMvc module added

--- Spring13-UnitTesting-MockMvc/src/main/java/com/mockmvc/entity/Student.java
+++ Spring13-UnitTesting-MockMvc/src/main/java/com/mockmvc/entity/Student.java
@@ -1,11 +1,23 @@
 package com.mockmvc.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     private String firstName;
     private String lastName;
---
+++ Spring13-UnitTesting-MockMvc/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into student (first_name,last_name,age) values ('Katya','Listsa',26)

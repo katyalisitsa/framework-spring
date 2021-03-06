fd58cd5d00148894120e0ccdc2a55f4cc3cbe766
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-03-06 13:29:28 -0500

    MockMvc module added

---
+++ Spring13-UnitTesting-MockMvc/src/test/java/com/mockmvc/controller/StudentControllerTest.java
@@ -0,0 +1,31 @@
+package com.mockmvc.controller;
+
+import com.mockmvc.service.StudentService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(StudentController.class)
+class StudentControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    StudentService studentService;
+
+    @Test
+    void getStudentData() throws Exception {
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/student").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                //.andExpect(content().json())
+                .andReturn();
+    }
+}

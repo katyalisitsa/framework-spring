0d189a11082f31413cc714de181bcd3de7de2fd7
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-03-06 11:00:47 -0500

    MockMvc module added

--- Spring13-UnitTesting-MockMvc/src/test/java/com/mockmvc/UnitTestingImplTest.java
+++ Spring13-UnitTesting-MockMvc/src/test/java/com/mockmvc/UnitTestingImplTest.java
@@ -7,6 +7,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,6 +45,14 @@ class UnitTestingImplTest {
     @Test
     void calculateSumUsingDataService_Mock_withParameter() {
         when(dataRepository.findById(2)).thenReturn(new int[]{10, 10, 10});
+        int actual = unitTesting.calculateSumUsingDataService_withParameter();
+        assertEquals(30, actual);
+
+    }
+
+    @Test
+    void calculateSumUsingDataService_Mock_withGenericParameter() {
+        when(dataRepository.findById(anyInt())).thenReturn(new int[]{10, 10, 10});
         int actual = unitTesting.calculateSumUsingDataService_withParameter();
         assertEquals(30, actual);
 

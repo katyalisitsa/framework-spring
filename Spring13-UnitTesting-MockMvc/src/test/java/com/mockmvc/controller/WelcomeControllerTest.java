5aca2a246006f4b37c00050176d32b635004d2e9
Kateryna Lysytsia <katya.lisitsa@gmail.com>
2021-03-06 11:30:25 -0500

    MockMvc module added

--- Spring13-UnitTesting-MockMvc/src/test/java/com/mockmvc/controller/WelcomeControllerTest.java
+++ Spring13-UnitTesting-MockMvc/src/test/java/com/mockmvc/controller/WelcomeControllerTest.java
@@ -1,14 +1,25 @@
 package com.mockmvc.controller;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @WebMvcTest(WelcomeController.class)
 class WelcomeControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void welcome() {
+    void welcome() throws Exception {
+
+        RequestBuilder request = MockMvcRequestBuilders.get("/welcome").accept(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(request).andReturn();
+
     }
 }
